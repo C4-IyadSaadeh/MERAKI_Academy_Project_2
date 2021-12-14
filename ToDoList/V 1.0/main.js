13b9b9d8ef6a9e03044945029498b28ba9f1086c
Iyad Saadeh <93885250+IyadSaadeh@users.noreply.github.com>
2021-12-14 23:16:46 +0200

    Finish Main Function

--- ToDoList/V 1.0/main.js
+++ ToDoList/V 1.0/main.js
@@ -23,9 +23,9 @@ registertab.on("click", () => {
   divlogin.hide();
   divregister.show();
 });
-// Local Storage 
-const storage=this.localStorage;
-let users=[];
+// Local Storage
+const storage = this.localStorage;
+let users = [];
 //login
 const sectionFirst = $(".first");
 const btnSignIn = $("#btnSignIn");
@@ -42,17 +42,15 @@ const tabContentLogin = $("#tab-content-login");
 const tabContentSignUp = $("#tab-content-signUp");
 //section second
 const hero = $("#hero");
-
+const sectionCards = $(".third");
 const body = $("body");
 const HideAll = () => {
-  
   for (const key in users) {
-      const element = users[key];
-      if (element.isActive===true) {
-        sectionFirst.hide();
-        hero.show();
-      }
-    
+    const element = users[key];
+    if (element.isActive === true) {
+      sectionFirst.hide();
+      hero.show();
+    }
   }
   divregister.hide();
   hero.hide();
@@ -60,7 +58,7 @@ const HideAll = () => {
 body.on("load", HideAll());
 btnSignIn.on("click", () => {
   // Don't Forget You have add SignOut Button.
-  users=JSON.parse(storage.getItem('users'));
+  users = JSON.parse(storage.getItem("users"));
   if (loginEmail.val() !== "" && loginPassword.val() !== "") {
     for (const key in users) {
       const element = users[key];
@@ -68,30 +66,33 @@ btnSignIn.on("click", () => {
         loginEmail.val() === element.email &&
         loginPassword.val() === element.password
       ) {
-        
         sectionFirst.hide();
         hero.show();
-      }
-      else if(loginEmail.val() !== element.email &&
-      loginPassword.val() !== element.password){
-        const err = $(`<h6 style="color:red;">*Your Email or password is un correct</h6>`);
+      } else if (
+        loginEmail.val() !== element.email &&
+        loginPassword.val() !== element.password
+      ) {
+        const err = $(
+          `<h6 style="color:red;">*Your Email or password is un correct</h6>`
+        );
 
         err.appendTo(tabContentLogin);
       }
     }
-  
   } else if (loginEmail.val() === "" && loginPassword.val() !== "") {
-    const err = $(`<h6 style="color:red;">*Please Fill email ex:xxxx@gmail.com</h6>`);
+    const err = $(
+      `<h6 style="color:red;">*Please Fill email ex:xxxx@gmail.com</h6>`
+    );
 
     err.appendTo(tabContentLogin);
-  }
-  else if (loginEmail.val() !== "" && loginPassword.val() === "") {
+  } else if (loginEmail.val() !== "" && loginPassword.val() === "") {
     const err = $(`<h6 style="color:red;">*Please Fill Password </h6>`);
 
     err.appendTo(tabContentLogin);
-  }
-  else  {
-    const err = $(`<h6 style="color:red;">*Please Fill email ex:xxxx@gmail.com and password</h6>`);
+  } else {
+    const err = $(
+      `<h6 style="color:red;">*Please Fill email ex:xxxx@gmail.com and password</h6>`
+    );
 
     err.appendTo(tabContentLogin);
   }
@@ -109,23 +110,28 @@ btnRegister.on("click", () => {
         firstName: registerFirstName.val(),
         lastName: registerLastName.val(),
         email: registerEmail.val(),
-        password: registerPassword.val(),isActive:true
+        password: registerPassword.val(),
+        isActive: true,
       });
       storage.setItem("users", JSON.stringify(users));
-      const err = $(`<h4>Welcome ${registerFirstName.val()} ${registerLastName.val()}</h4>`);
+      const err = $(
+        `<h4>Welcome ${registerFirstName.val()} ${registerLastName.val()}</h4>`
+      );
 
-        err.appendTo(tabContentSignUp);
-      
+      err.appendTo(tabContentSignUp);
     } else if (registerPassword.val() !== registerConfirmPassword.val()) {
-      const err = $(`<h6 style="color:red;">*The Password not match for Confirm Password Please Try Agin!</h6>`);
+      const err = $(
+        `<h6 style="color:red;">*The Password not match for Confirm Password Please Try Agin!</h6>`
+      );
 
       err.appendTo(tabContentSignUp);
     }
-  }
-  else{
-    const err = $(`<h6 style="color:red;">*Please Fill the field and Try Agin!</h6>`);
+  } else {
+    const err = $(
+      `<h6 style="color:red;">*Please Fill the field and Try Agin!</h6>`
+    );
 
-      err.appendTo(tabContentSignUp);
+    err.appendTo(tabContentSignUp);
   }
 });
 arabic.on("click", () => {
@@ -152,15 +158,14 @@ lightSpan.on("click", () => {
   // $('link #dark').eq(1).remove();
 });
 
-// main function 
+// main function
 // 1. User should be able to add a new toDo item to the toDos list.
 // 2.User should be able to view all added items.
-let items=[];
+let items = [];
 
-const viewList=()=>{
-  items=JSON.parse(storage.getItem('items'));
-  for (const key in items) {
-    const element = items[key];
+const viewList = () => {
+  items = JSON.parse(storage.getItem("items"));
+  items.forEach((element) => {
     const card = $('<div class="card"></div>');
     const cardHeader = $('<div class="card-header"></div>');
     const cardBody = $('<div class="card-body"></div>');
@@ -171,19 +176,55 @@ const viewList=()=>{
       `<div><h3>${element.title}</h3><p>${element.Description}</p></div>`
     );
     // const footerOne = $(`<button id="modifyStep">Add Step</button>`);
-    const footertwo = $(`<button id="update">Edit</button>`);
-    const footerthree = $(`<button id="delete">Delete</button>`);
+    const updatebtn = $(`<button id="update">Edit</button>`);
+    const deletebtn = $(`<button id="delete">Delete</button>`);
     header.appendTo(cardHeader);
     // footerOne.appendTo(cardFooter);
-    footertwo.appendTo(cardFooter);
-    footerthree.appendTo(cardFooter);
+    updatebtn.appendTo(cardFooter);
+    deletebtn.appendTo(cardFooter);
     cardHeader.appendTo(card);
     cardBody.appendTo(card);
     cardFooter.appendTo(card);
     card.appendTo(thridSection);
-  }
+    // 3.User should be able to delete any added item.
+    deletebtn.on("click", () => {
+      items.forEach((item, index) => {
+        if (element.title === item.title) {
+          items.splice(index, 1);
+        }
+      });
+      storage.setItem("items", JSON.stringify(items));
+      card.remove();
+    });
+    // 4. User should be able to modify any added item.
+    updatebtn.on("click", () => {
+      items.forEach((item, index) => {
+        if (element.title === item.title) {
+          const title = $(`<input type="text" value="${item.title}" />`);
+          const des = $(`<textarea>${item.Description}</textarea>`);
+          const from = $(`<input type="date" value="${item.from}"/>`);
+          const to = $(`<input type="date" value="${item.to}"/>`);
+          const editDiv = $('<div class="card_edit"></div>');
+          title.appendTo(editDiv);
+          des.appendTo(editDiv);
+          from.appendTo(editDiv);
+          to.appendTo(editDiv);
+          editDiv.appendTo(sectionCards);
+          items[index] = {
+            title: title.val(),
+            Description: des.val(),
+            from: from.val(),
+            to: to.val(),
+            state: "Pending",
+          };
+        }
+      });
+      storage.setItem("items", JSON.stringify(items));
+      viewList();
+    });
+  });
 };
-btnFinish.on('click',()=>{
+btnFinish.on("click", () => {
   if (
     titleIdea.val() !== "" &&
     descriptionIdea.val() !== "" &&
@@ -191,14 +232,14 @@ btnFinish.on('click',()=>{
     toDate.val() !== ""
   ) {
     items.push({
+      // id:items.length,
       title: titleIdea.val(),
       Description: descriptionIdea.val(),
       from: fromDate.val(),
-      to: toDate.val(),state:'Pending'
+      to: toDate.val(),
+      state: "Pending",
     });
     storage.setItem("items", JSON.stringify(items));
-viewList();
+    viewList();
   }
 });
-
-// 3.User should be able to delete any added item.
