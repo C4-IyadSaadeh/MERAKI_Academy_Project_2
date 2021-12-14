95bc698efef0bb176394c40f9a1dc2358b68c607
Iyad Saadeh <93885250+IyadSaadeh@users.noreply.github.com>
2021-12-14 21:42:51 +0200

    Finished Login And Register
    Finsh Functions and Make Vaildate For All Field

--- ToDoList/V 1.0/main.js
+++ ToDoList/V 1.0/main.js
@@ -23,7 +23,9 @@ registertab.on("click", () => {
   divlogin.hide();
   divregister.show();
 });
-
+// Local Storage 
+const storage=this.localStorage;
+let users=[];
 //login
 const sectionFirst = $(".first");
 const btnSignIn = $("#btnSignIn");
@@ -36,29 +38,94 @@ const registerLastName = $("#registerLastName");
 const registerEmail = $("#registerEmail");
 const registerPassword = $("#registerPassword");
 const registerConfirmPassword = $("#registerConfirmPassword");
+const tabContentLogin = $("#tab-content-login");
+const tabContentSignUp = $("#tab-content-signUp");
 //section second
 const hero = $("#hero");
 
 const body = $("body");
 const HideAll = () => {
+  
+  for (const key in users) {
+      const element = users[key];
+      if (element.isActive===true) {
+        sectionFirst.hide();
+        hero.show();
+      }
+    
+  }
   divregister.hide();
   hero.hide();
 };
 body.on("load", HideAll());
 btnSignIn.on("click", () => {
+  // Don't Forget You have add SignOut Button.
+  users=JSON.parse(storage.getItem('users'));
   if (loginEmail.val() !== "" && loginPassword.val() !== "") {
-    for (const key in user) {
-      const element = user[key];
+    for (const key in users) {
+      const element = users[key];
       if (
         loginEmail.val() === element.email &&
         loginPassword.val() === element.password
       ) {
+        
         sectionFirst.hide();
         hero.show();
       }
+      else if(loginEmail.val() !== element.email &&
+      loginPassword.val() !== element.password){
+        const err = $(`<h6 style="color:red;">*Your Email or password is un correct</h6>`);
+
+        err.appendTo(tabContentLogin);
+      }
     }
-  } else {
-    alert("Please fill your email or password");
+  
+  } else if (loginEmail.val() === "" && loginPassword.val() !== "") {
+    const err = $(`<h6 style="color:red;">*Please Fill email ex:xxxx@gmail.com</h6>`);
+
+    err.appendTo(tabContentLogin);
+  }
+  else if (loginEmail.val() !== "" && loginPassword.val() === "") {
+    const err = $(`<h6 style="color:red;">*Please Fill Password </h6>`);
+
+    err.appendTo(tabContentLogin);
+  }
+  else  {
+    const err = $(`<h6 style="color:red;">*Please Fill email ex:xxxx@gmail.com and password</h6>`);
+
+    err.appendTo(tabContentLogin);
+  }
+});
+btnRegister.on("click", () => {
+  if (
+    registerFirstName.val() !== "" &&
+    registerLastName.val() !== "" &&
+    registerEmail.val() !== "" &&
+    registerPassword.val() !== "" &&
+    registerConfirmPassword.val() !== ""
+  ) {
+    if (registerPassword.val() === registerConfirmPassword.val()) {
+      users.push({
+        firstName: registerFirstName.val(),
+        lastName: registerLastName.val(),
+        email: registerEmail.val(),
+        password: registerPassword.val(),isActive:true
+      });
+      storage.setItem("users", JSON.stringify(users));
+      const err = $(`<h4>Welcome ${registerFirstName.val()} ${registerLastName.val()}</h4>`);
+
+        err.appendTo(tabContentSignUp);
+      
+    } else if (registerPassword.val() !== registerConfirmPassword.val()) {
+      const err = $(`<h6 style="color:red;">*The Password not match for Confirm Password Please Try Agin!</h6>`);
+
+      err.appendTo(tabContentSignUp);
+    }
+  }
+  else{
+    const err = $(`<h6 style="color:red;">*Please Fill the field and Try Agin!</h6>`);
+
+      err.appendTo(tabContentSignUp);
   }
 });
 arabic.on("click", () => {
@@ -72,28 +139,7 @@ english.on("click", () => {
   $("#ar").remove();
   // $('link #ar').eq(1).remove();
 });
-btnRegister.on("click", () => {
-    if (
-      registerFirstName.val() !== "" &&
-      registerLastName.val() !== "" &&
-      registerEmail.val() !== "" &&
-      registerPassword.val() !== "" &&
-      registerConfirmPassword.val() !== ""
-    ) {
-      if (registerPassword.val() === registerConfirmPassword.val()) {
-        users.push({
-          firstName: registerFirstName.val(),
-          lastName: registerLastName.val(),
-          email: registerEmail.val(),
-          password: registerPassword.val(),
-        });
-        data.setItem("users", JSON.stringify(users));
-        alert(`Welcome ${registerFirstName.val()} ${registerLastName.val()}`);
-      } else if(registerPassword.val() !== registerConfirmPassword.val()) {
-        alert("The Password not match for Confirm Password Please Try Agin!");
-      }
-    }
-  });
+
 darkSpan.on("click", () => {
   // linkStyle.attr('href','arabicStyle.css');
   // html.attr('lang','ar');
@@ -105,3 +151,7 @@ lightSpan.on("click", () => {
   $("#dark").remove();
   // $('link #dark').eq(1).remove();
 });
+
+// main function 
+// 1. User should be able to add a new toDo item to the toDos list.
+
